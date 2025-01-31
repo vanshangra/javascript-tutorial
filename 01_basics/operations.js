b0ef5fd7cf7ba40e903128a0147db512e4154af9
Vansh Angra <146015188+vanshangra@users.noreply.github.com>
2025-01-31 18:55:48 +0000

    conversion of datatypes

---
+++ 01_basics/operations.js
@@ -0,0 +1,28 @@
+let score = "vansh"
+
+//const {score} = req.score
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false =>0
+
+let isloggedIn = "vansh"
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 =>true; 0 => false
+//"" => false
+//"vansh" => true
+
+let someNumber =33
+
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
