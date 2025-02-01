bfeeb4233a0de7aae43711eca8347944a92a0492
Vansh Angra <146015188+vanshangra@users.noreply.github.com>
2025-02-01 17:44:58 +0000

    operartions and comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null>0);
+console.log(null == 0);
+console.log(null>=0);
+//avoid these types of comparisons
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === strict check, also checks the datatypes.
+
+console.log("2" === 2);
