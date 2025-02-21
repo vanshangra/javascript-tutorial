ad15f7095714cb2f523b1202f075ca5462de244e
Vansh Angra <146015188+vanshangra@users.noreply.github.com>
2025-02-21 18:27:06 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,18 @@
+//singleton
+//Object.create()
+
+//object literals
+const Jsuser = {
+    name: "Vansh",
+    "full name": "Vansh Angra"
+    age: 19,
+    location: "Vasundhara",
+    email: "vanshgmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday","Sunday"]
+}
+
+console.log(Jsuser.email);
+console.log(Jsuser["email"]);
+console.log(Jsuser["full name"]);
+//3:53:17
