50edbd0acfa6d196d1e02ad909a6f12765b92c56
Vansh Angra <146015188+vanshangra@users.noreply.github.com>
2025-02-22 11:10:20 +0000

    objects in js

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,9 +2,13 @@
 //Object.create()
 
 //object literals
+
+const mySym = Symbol("key1")
+
 const Jsuser = {
     name: "Vansh",
-    "full name": "Vansh Angra"
+    "full name": "Vansh Angra",
+    [mySym]: "mykey1",
     age: 19,
     location: "Vasundhara",
     email: "vanshgmail.com",
@@ -12,7 +16,25 @@ const Jsuser = {
     lastLoginDays: ["Monday","Sunday"]
 }
 
-console.log(Jsuser.email);
-console.log(Jsuser["email"]);
-console.log(Jsuser["full name"]);
+// console.log(Jsuser.email);
+// console.log(Jsuser["email"]);
+// console.log(Jsuser["full name"]);
+// console.log(Jsuser[mySym]);
 //3:53:17
+
+Jsuser.email = "vansh@chatgpt.com"
+//Object.freeze(Jsuser)
+Jsuser.email = "vansh@gpt.com"
+//console.log(Jsuser);
+
+Jsuser.greeting = function(){
+    console.log("hello JS user"); 
+}
+Jsuser.greetingTwo = function(){
+    console.log(`hello JS user, ${this.name}`); 
+}
+
+console.log(Jsuser.greeting());
+console.log(Jsuser.greetingTwo());
+
+
