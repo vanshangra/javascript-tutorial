ebfef4fd3796807852d63e291a19b4c90cf47ecb
Vansh Angra <146015188+vanshangra@users.noreply.github.com>
2025-02-26 18:00:48 +0000

    higher order array loops, filter, map and reduce

---
+++ 05_iterations/four.js
@@ -0,0 +1,29 @@
+const myObject = {
+    js: "javascript",
+    cpp: 'c++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+//for in loop
+for (const key in myObject) {
+    // console.log(`${key} for ${myObject[key]}`);
+}
+
+const programming =['js','ruby','python','cpp','java']
+
+for (const key in programming) {
+//    console.log(programming[key]);
+   
+}
+
+// const map = new Map()
+// map.set('IN',"India")
+// map.set('USA',"United States of America")
+// map.set('Fr',"France")
+// map.set('IN',"India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
